8d81a5861a925c1c64f2c65bd3859b630f0b8385
swapnil <swapnilghule112@gmail.com>
2019-11-17 00:44:00 +0530

    Completed part2 of tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<h1>Welcome to my blog</h1>"
+    user = {'username': 'Swapnil'}
+    posts = [
+        {
+            'author': {'username': 'vince gilligan'},
+            'body': 'Breaking Bad!'
+        },
+        {
+            'author': {'username': 'Walter White'},
+            'body': 'How to make meth , 99.9 percent pure?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
